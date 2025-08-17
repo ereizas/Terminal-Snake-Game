fa324de861adcd5e282314f219de44a2973972f7
ereizas <ereizas@gmail.com>
2025-08-17 09:06:26 -0400

    draw top of board

--- snake_game.c
+++ snake_game.c
@@ -4,7 +4,16 @@
 #define GRID_LENGTH 20
 #define GRID_WIDTH 20
 
+void print_board(char board[GRID_LENGTH][GRID_WIDTH], int grid_length, int grid_width)
+{
+    for(int i = 0; i<grid_width; ++i){
+        printf("%c",'_');
+    }
+    puts("");
+}
+
 int main(int argc, char const *argv[])
 {
+    print_board(NULL, GRID_LENGTH, GRID_WIDTH);
     return 0;
 }
