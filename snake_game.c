00d1d1da0bda2ad107d44bc49f326185d2e92216
ereizas <ereizas@gmail.com>
2025-08-17 09:26:42 -0400

    implement printing of initial board

--- snake_game.c
+++ snake_game.c
@@ -4,16 +4,34 @@
 #define GRID_LENGTH 20
 #define GRID_WIDTH 20
 
-void print_board(char board[GRID_LENGTH][GRID_WIDTH], int grid_length, int grid_width)
-{
-    for(int i = 0; i<grid_width; ++i){
-        printf("%c",'_');
+void print_horizontal_border(int grid_width){
+    printf("%c",' ');
+    for(int i = 0; i<grid_width; ++i)
+    {
+        printf("%s","- ");
     }
     puts("");
 }
 
-int main(int argc, char const *argv[])
-{
-    print_board(NULL, GRID_LENGTH, GRID_WIDTH);
+void print_board(char board[GRID_LENGTH][GRID_WIDTH], int grid_length, int grid_width){
+    print_horizontal_border(grid_width);
+    for(int i = 0; i<grid_length; ++i){
+        printf("%c",'|');
+        for(int j = 0; j<grid_width; ++j){
+            printf("%c|",board[i][j]);
+        }
+        puts("");
+        print_horizontal_border(grid_width);
+    }
+}
+
+int main(int argc, char const *argv[]){
+    char board[GRID_LENGTH][GRID_WIDTH];
+    for(int i = 0; i<GRID_LENGTH; ++i){
+        for(int j = 0; j<GRID_WIDTH; ++j){
+            board[i][j] = ' ';
+        }
+    }
+    print_board(board, GRID_LENGTH, GRID_WIDTH);
     return 0;
 }
